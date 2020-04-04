6ba95eda35471529d45bccb47481f09f2519fbd5
rcantero <60898355+rcantero@users.noreply.github.com>
2020-04-04 13:03:47 +0200

    Add files via upload

--- APIC-EM-DEV-NET.py
+++ APIC-EM-DEV-NET.py
@@ -9,21 +9,15 @@ from pprint import pprint
 def obtencion_ticket():
     requests.packages.urllib3.disable_warnings()
     url="https://devnetsbx-netacad-apicem-3.cisco.com/api/v1/ticket"
-    headers={
-        "Content-Type":"aplication/json"
-        }
-
-    body_json={
-        "password": "Xj3BDqbU",
-        "username": "devnetuser"}
-
+    headers={"Content-Type":"application/json"}
+    body_json={"password": "Xj3BDqbU","username": "devnetuser"}
     resp =requests.post(url,json.dumps(body_json),headers=headers,verify=False)
-
-    print("Peticion ticket con codigo ",resp.status_code)
-
     respuesta_json= resp.json()
-    pprint(respuesta_json)
+    global ticket
+    ticket=respuesta_json["response"]["serviceTicket"]
+        
 
 
-
+ticket=()
 obtencion_ticket()
+print ("tk fuera",ticket)
