94d81d258965509704beca9e06c7e82411429212
rcantero <60898355+rcantero@users.noreply.github.com>
2020-04-06 15:43:27 +0200

    Add files via upload

--- APIC-EM-DEV-NET.py
+++ APIC-EM-DEV-NET.py
@@ -15,10 +15,6 @@ def obtencion_ticket():
     respuesta_json= resp.json()
     global ticket
     ticket=respuesta_json["response"]["serviceTicket"]
-        
-#ticket=()
-obtencion_ticket()
-#print ("tk fuera",ticket)
 
 def peticion(do):
     requests.packages.urllib3.disable_warnings()
@@ -28,78 +24,80 @@ def peticion(do):
     resp =requests.get(url,headers=headers,verify=False)
     global respuesta_json
     respuesta_json= resp.json()
-    #pprint(respuesta_json)
-    cont=0
-    print("Los equipos de red disponibles son: ")
-    global lista_nom
-    lista_nom={}
-
-    for equipo in respuesta_json["response"]:
-        #print(respuesta_json["response"][cont]["hostname"])
-        lista_nom[respuesta_json["response"][cont]["hostname"]]=cont
-        cont+=1
+    if do == "network-device":
+        print("Los equipos de red disponibles son: ")
+        lista_nom={}
+        cont=0
+        for equipo in respuesta_json["response"]:
+            lista_nom[respuesta_json["response"][cont]["hostname"]]=cont
+            cont+=1
+        for key,value in lista_nom.items():
+            print(value,"-",key)
+        return lista_nom
     
-    
-
-    #print("interior de lista", lista_nom)
-
-    for key,value in lista_nom.items():
-        print(value,"-",key)
-    
-
-
-    #global cual
-    #cual=input("¿De cual de ellos quieres saber la informacion? Introduce su numero asociado o el nombre exacto\n")
-    
-
-    #for equipo in respuesta_json["response"]:
-        #print("El dispositivo", respuesta_json["response"][cont]["hostname"]+" es un",
-        #respuesta_json["response"][cont]["type"],"version",respuesta_json["response"][cont]["softwareVersion"] ,"y su funcion es actuar como ",respuesta_json["response"][cont]["role"],"teniendo como ip de gestion:",respuesta_json["response"][cont]["managementIpAddress"])
-        #cont+=1
-
-
-def quin(cual):
-     
-    for key,value in lista_nom.items():
-        if cual == str(value):
-            que_equipo=lista_nom[key]
-            
-            return que_equipo
-        elif  cual == key:
-            que_equipo=lista_nom[key]
-            return que_equipo
-                
-
-    
+    if do == "host":
+        print("Los host en la red son: ")
+        lista_nom={}
+        cont=0
+        for equipo in respuesta_json["response"]:
+            lista_nom[respuesta_json["response"][cont]["hostIp"]]=cont
+            cont+=1
+        for key,value in lista_nom.items():
+            print(value,"-",key)
+        return lista_nom
+    else:
+        print("invalido")
         
+def quin(cual,lst):
+    for key,value in lst.items():
+        que_equipo=lst[key]
+        
+        if cual == str(value):
+            if do == "network-device":
+                print("El dispositivo", respuesta_json["response"][que_equipo]["hostname"]+" es un",
+                respuesta_json["response"][que_equipo]["type"],"version",respuesta_json["response"][que_equipo]["softwareVersion"] ,"y su funcion es actuar como ",respuesta_json["response"][que_equipo]["role"],"teniendo como ip de gestion:",respuesta_json["response"][que_equipo]["managementIpAddress"])
+                return que_equipo
+            elif do == "host":
+                if respuesta_json["response"][que_equipo]["hostType"] =="wireless":
+                    print("El dispositivo", respuesta_json["response"][que_equipo]["hostIp"]+" tiene la mac ",respuesta_json["response"][que_equipo]["hostMac"]+ "y esta en la vlan "+ respuesta_json["response"][que_equipo]["vlanId"], "conectado por ", respuesta_json["response"][que_equipo]["hostType"], "a el dispositivo de red ",respuesta_json["response"][que_equipo]["connectedAPName"] )
+                    return que_equipo
+                else :
+                    print("El dispositivo", respuesta_json["response"][que_equipo]["hostIp"]+" tiene la mac ",respuesta_json["response"][que_equipo]["hostMac"]+ "y esta en la vlan "+ respuesta_json["response"][que_equipo]["vlanId"], "conectado por ", respuesta_json["response"][que_equipo]["hostType"], "a el dispositivo de red con ip",respuesta_json["response"][que_equipo]["connectedNetworkDeviceIpAddress"], "en la interfaz",respuesta_json["response"][que_equipo]["connectedInterfaceName"]  )
+                    return que_equipo
+            
 
-    
+        elif  cual == key:
+            if do == "network-device":
+                print("El dispositivo", respuesta_json["response"][que_equipo]["hostname"]+" es un",
+                respuesta_json["response"][que_equipo]["type"],"version",respuesta_json["response"][que_equipo]["softwareVersion"] ,"y su funcion es actuar como ",respuesta_json["response"][que_equipo]["role"],"teniendo como ip de gestion:",respuesta_json["response"][que_equipo]["managementIpAddress"])
+                return que_equipo
+            elif do == "host":
+                if respuesta_json["response"][que_equipo]["hostType"] =="wireless":
+                    print("El dispositivo", respuesta_json["response"][que_equipo]["hostIp"]+" tiene la mac ",respuesta_json["response"][que_equipo]["hostMac"]+ "y esta en la vlan "+ respuesta_json["response"][que_equipo]["vlanId"], "conectado por ", respuesta_json["response"][que_equipo]["hostType"], "a el dispositivo de red ",respuesta_json["response"][que_equipo]["connectedAPName"] )
+                    return que_equipo
+                else :
+                    print("El dispositivo", respuesta_json["response"][que_equipo]["hostIp"]+" tiene la mac ",respuesta_json["response"][que_equipo]["hostMac"]+ "y esta en la vlan "+ respuesta_json["response"][que_equipo]["vlanId"], "conectado por ", respuesta_json["response"][que_equipo]["hostType"], "a el dispositivo de red con ip",respuesta_json["response"][que_equipo]["connectedNetworkDeviceIpAddress"], "en la interfaz",respuesta_json["response"][que_equipo]["connectedInterfaceName"]  )
+                      
+def inicio(que):
+    if que== "red":        
+        return "network-device"
+    if que== "host":        
+        return "host"
+    else:
+        que=input("El dispositivo tipo de dispositivo introducido no es valido. Introduce *host* o *red* \n")
+        return inicio(que)
 
+obtencion_ticket()
+que=input("Que deseas tipo de dispositivo deseas revisar host o red?:")
+do =inicio(que)
+exi=peticion(str(do))
 
-
-    #print("los valores son",lista_nom.items())
-    #que_equipo=lista_nom[cual]
-    #print("numero en la lista equipos es \n",que_equipo)
-
-    #print("El dispositivo", respuesta_json["response"][que_equipo]["hostname"]+" es un",
-   #     respuesta_json["response"][que_equipo]["type"],"version",respuesta_json["response"][que_equipo]["softwareVersion"] ,"y su funcion es actuar como ",respuesta_json["response"][que_equipo]["role"],"teniendo como ip de gestion:",respuesta_json["response"][que_equipo]["managementIpAddress"])
-
-
-
-do=input("que deseas hacer?:")
-
-do="network-device" ### esto ira fuera 
-
-peticion(do)
-
-cual=input("¿De cual de ellos quieres saber la informacion? Introduce su numero asociado o el nombre exacto: \n")
-
-que_equipo=quin(cual)
+cual=input("¿De cual de ellos quieres saber la informacion? Introduce su numero asociado o su identificacion exacta: \n")
+que_equipo=quin(cual,exi)
 
 while que_equipo == None:
-    cual=input("Has introducido mal el equipo. Introduce su numero asociado o el nombre exacto\n")
-    que_equipo=quin(cual)
+                cual=input("Has introducido mal el equipo. Introduce su numero asociado o su identificacion exacta: \n")
+                que_equipo=quin(cual,exi)
 
-
-print("El dispositivo", respuesta_json["response"][que_equipo]["hostname"]+" es un",
-respuesta_json["response"][que_equipo]["type"],"version",respuesta_json["response"][que_equipo]["softwareVersion"] ,"y su funcion es actuar como ",respuesta_json["response"][que_equipo]["role"],"teniendo como ip de gestion:",respuesta_json["response"][que_equipo]["managementIpAddress"])
+            
+            
